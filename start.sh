d9d3b2c1b409c4fdd12189a5fd972c2fcdd4b711
airstudent2 <rmondal61955@gmail.com>
2025-12-17 20:40:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
-
 #!/bin/bash
-
-# 1. Bot start করো ব্যাকগ্রাউন্ডে (& চিহ্ন খুব জরুরি)
-python -m bot.main &
-
-# 2. Flask Server start করো
-gunicorn server.app:app --bind 0.0.0.0:$PORT
+python -m bot.main
